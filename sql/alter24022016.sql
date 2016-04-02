6d2b3244c201817270d7589376484e1d33358f7e
anph1991 <phamhoaian005@gmail.com>
2016-04-02 12:09:48 +0700

    DB - create new table for Blog categories (updated)

--- sql/alter24022016.sql
+++ sql/alter24022016.sql
@@ -1,16 +1,16 @@
 
 -- Date: 24/02/2016
--- Commit message: DB - create new table for Blog
+-- Commit message: DB - create new table for Blog categories
 
 CREATE TABLE IF NOT EXISTS `blog_categories` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `parent` int(11) DEFAULT '0',
+  `description` mediumtext,
+  `parent` int(11) NOT NULL DEFAULT '0',
   `published` smallint(1) NOT NULL DEFAULT '0',
   `ordering` int(11) NOT NULL DEFAULT '0',
-  `image` varchar(255) NOT NULL,
+  `image` tinytext,
   `delete_flag` smallint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
